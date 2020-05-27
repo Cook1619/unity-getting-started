eb561cbd8465f190cf8632cf667665fb5a6b4d5d
Cook1619 <cook6348@gmail.com>
2020-05-26 19:44:55 -0500

    first commit

---
+++ Assets/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string itemName = "Axe";
+    public string itemDesc = "Doubled Edged Axe";
+    public int attackStrength = 25;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(new Vector(15,15,15) * Time.deltaTime);
+    }
+}
---
+++ Assets/Scenes/NewBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehavior : MonoBehaviour
+{
+    public string name = "Matt";
+    public int age = 33;
+    public float mySpeed = 10.2f;
+    public int myHealth = 100;
+    public int score = 50;
+    public bool hasAllKeys = false;
+    public int ammoCount = 75;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("My name is " + name);
+        Debug.Log("My age is " + age);
+        Debug.Log("My speed is " + mySpeed);
+        Debug.Log("My health is " + myHealth);
+        Debug.Log("My score is " + score);
+        Debug.Log("Does the user have all the key? " + hasAllKeys);
+        Debug.Log("Ammo count: " + ammoCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
